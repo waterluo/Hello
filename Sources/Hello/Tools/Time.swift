df656f2f5064ae4d6c863a66d629cf2713f51f51
waterluo <shuishengluo@gmail.com>
2022-05-20 22:32:20 +0800

    Update Time.swift

--- Sources/Hello/Tools/Time.swift
+++ Sources/Hello/Tools/Time.swift
@@ -6,14 +6,14 @@
 //
 
 import Foundation
+import QuartzCore
 
 public struct Time {
     public static func test(_ title: String,_ task: ()->Void) {
-        print(title)
-        let begin = CFAbsoluteTimeGetCurrent()
+        let begin = CACurrentMediaTime() // 秒为单位
         task()
-        let end = CFAbsoluteTimeGetCurrent()
+        let end = CACurrentMediaTime()
         let delta = (end - begin)
-        print("耗时：\(String(format: "%.3f", delta * 1000))毫秒")
+        print("[\(title)] 耗时：\(String(format: "%.3f", delta * 1000))毫秒")
     }
 }
