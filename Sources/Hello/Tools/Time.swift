99dbdf7eeabc778dccbd7725ed96b5c79a0fa41f
waterluo <shuishengluo@gmail.com>
2022-05-20 22:04:16 +0800

    Create Time.swift

---
+++ Sources/Hello/Tools/Time.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by sheng on 2022/5/20.
+//
+
+import Foundation
+
+struct Time {
+    static func test(_ title: String,_ task: ()->Void) {
+        print(title)
+        let begin = CFAbsoluteTimeGetCurrent()
+        task()
+        let end = CFAbsoluteTimeGetCurrent()
+        let delta = (end - begin)
+        print("耗时：\(String(format: "%.3f", delta * 1000))毫秒")
+    }
+}
